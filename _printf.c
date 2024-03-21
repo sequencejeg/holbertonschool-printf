452511cb5e091782be31d68c8837221c20614510
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-03-21 19:24:14 -0400

    update

---
+++ _printf.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int _printf(const char* format, ...)
+{
+
+	va_list arguments;
+
+	so_t conversion[] = { 
+		{"c", print_character}, 
+		{"s", print_string}, 
+		{"%", print_percent},
+		}
+
+	int i, j, counter;
+	 
+	va_start(arguments, format);
+
+
+
+}
+
+
+
+
+	return ();
+}
+
